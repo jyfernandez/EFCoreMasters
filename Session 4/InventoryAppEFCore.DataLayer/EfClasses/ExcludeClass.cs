58441ee1bc8d000308afb5fd6a74e3a277d2c089
jyfernandez <jydfernandez@gmail.com>
2023-07-26 18:03:03 +0800

    Implement the configurations for non-relational properties.

---
+++ Session 4/InventoryAppEFCore.DataLayer/EfClasses/ExcludeClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryAppEFCore.DataLayer.EfClasses
+{
+    [NotMapped]
+    public class ExcludeClass
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ Session 4/InventoryAppEFCore.DataLayer/EfClasses/Review.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryAppEFCore.DataLayer.EfClasses
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewId { get; set; }
+        [MaxLength(50)]
+        [Required]
+        public string VoterName { get; set; }
+
+        private string _comment;
+
+        public string Comment => _comment;
+        public int NumStars { get; set; }
+
+        public int ProductId { get; set; }
+    }
+}
